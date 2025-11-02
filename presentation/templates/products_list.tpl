290d6565ccef1e2ef7f1060b7eac22d3d6fe7da7
AugustinCpu <augustin.mercurise@gmail.com>
2025-11-02 23:24:25 +0800

    modification du fichier products_list.tpl avec des classes bootstrap

--- presentation/templates/products_list.tpl
+++ presentation/templates/products_list.tpl
@@ -1,60 +1,92 @@
-{* products_list.tpl *}
+{* products_list.tpl - Version Bootstrap *}
 {load_presentation_object filename="products_list" assign="obj"}
 
+{* ------------------ SECTION PAGINATION ------------------ *}
 {if $obj->mrTotalPages > 1}
-<p class="d-flex">
-Page {$obj->mPage} of {$obj->mrTotalPages}
-{if $obj->mLinkToPreviousPage}
-<a href="{$obj->mLinkToPreviousPage}">Précédent</a>
-{else}
-Précédent
-{/if}
-{if $obj->mLinkToNextPage}
-<a href="{$obj->mLinkToNextPage}">Suivant</a>
-{else}
-Suivant
-{/if}
+<p class="d-flex justify-content-center align-items-center my-3">
+    <span class="me-3">Page {$obj->mPage} de {$obj->mrTotalPages}</span>
+    
+    <span class="mx-2">
+    {if $obj->mLinkToPreviousPage}
+        <a href="{$obj->mLinkToPreviousPage}" class="btn btn-outline-secondary btn-sm">Précédent</a>
+    {else}
+        <span class="btn btn-outline-secondary btn-sm disabled">Précédent</span>
+    {/if}
+    </span>
+    
+    <span class="mx-2">
+    {if $obj->mLinkToNextPage}
+        <a href="{$obj->mLinkToNextPage}" class="btn btn-outline-secondary btn-sm">Suivant</a>
+    {else}
+        <span class="btn btn-outline-secondary btn-sm disabled">Suivant</span>
+    {/if}
+    </span>
 </p>
 {/if}
 
+{* ------------------ SECTION LISTE DES PRODUITS (GRILLE) ------------------ *}
 {if $obj->mProducts}
-<div class="row gap-1">
-{section name=k loop=$obj->mProducts}
-{if $smarty.section.k.index % 2 == 0}
-{/if}
-<div class="card col-lg-5">
-  <div>
-    <h3 class="display-6 fs-4">
-    <a href="{$obj->mProducts[k].link_to_product}" class="text-decoration-none text-secondary">
-      {$obj->mProducts[k].name}
-    </a>
-  </h3>
-  </div>
-  <div class="d-flex gap-2">
-    {if $obj->mProducts[k].thumbnail neq ""}
-    <a href="{$obj->mProducts[k].link_to_product}">
-      {* <img src="{$obj->mProducts[k].thumbnail}" alt="{$obj->mProducts[k].name}" />*}
-    <img src="https://mdbootstrap.com/img/new/avatars/8.jpg" alt="" style="width: 109px; height: 150px; margin-right: 16px;" />
-    </a>
-    {/if}
-    <div class="d-flex flex-column">
-        <p>{$obj->mProducts[k].description}</p>
-        <p class="section">
-            Prix:
-            {if $obj->mProducts[k].discounted_price != 0}
-            <span class="old-price">{$obj->mProducts[k].price}</span>
-            <span class="price">{$obj->mProducts[k].discounted_price}</span>
-            {else}
-            <span class="price">{$obj->mProducts[k].price}</span>
-            {/if}
-        </p>
-    </div>
-  </div>
-</div>
-{if $smarty.section.k.index % 2 != 0 && !$smarty.section.k.first ||
-    $smarty.section.k.last}
-{/if}
-{/section}
-</div>
+{* Démarrer la ligne (row) de Bootstrap pour la grille. Utiliser row-cols-md-3 pour 3 produits par ligne (ex.) *}
+<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
+    
+    {section name=k loop=$obj->mProducts}
+        {* Chaque itération de boucle est une colonne/carte *}
+        <div class="col">
+            <div class="card h-100 shadow-sm product-card-custom">
+                
+                {* Conteneur d'image et badge *}
+                <div class="position-relative">
+                    {if $obj->mProducts[k].thumbnail neq ""}
+                        <a href="{$obj->mProducts[k].link_to_product}">
+                            {* Utiliser la variable Smarty pour l'image réelle, pas le placeholder *}
+                            {*<img src="{$obj->mProducts[k].thumbnail}" class="card-img-top" alt="{$obj->mProducts[k].name|escape:'html'}">*}
+                            <img src="https://placehold.co/600x500" class="card-img-top" alt="T-Shirt Dragon Rouge">
+                        </a>
+                        
+                        {* Logique du badge Solde (ajustée pour un pourcentage simple) *}
+                        {if $obj->mProducts[k].discounted_price != 0 && $obj->mProducts[k].price > $obj->mProducts[k].discounted_price}
+                            <span class="badge bg-danger position-absolute top-0 end-0 mt-2 me-2">
+                                SOLDE!
+                            </span>
+                        {/if}
+                    {/if}
+                </div>
+                
+                {* Corps de la carte (informations) *}
+                <div class="card-body d-flex flex-column text-center m-0 p-1">
+                    <h5 class="card-title fs-5 m-1">
+                        <a href="{$obj->mProducts[k].link_to_product}" class="text-decoration-none text-dark">
+                            {$obj->mProducts[k].name}
+                        </a>
+                    </h5>
+                    
+                    {* La description est souvent trop longue pour la carte - la remplacer par la catégorie serait mieux *}
+                    <p class="card-text text-muted small text-truncate m-0">
+                        {$obj->mProducts[k].description}
+                    </p>
+                    
+                    {* Section Prix et Bouton *}
+                    <div class="mt-auto">
+                        <div class="price-section d-flex">
+                            {if $obj->mProducts[k].discounted_price != 0}
+                                {* Prix barré *}
+                                <span class="text-muted text-decoration-line-through me-2 fs-5">{$obj->mProducts[k].price|string_format:"%.2f €"}</span>
+                                {* Nouveau prix *}
+                                <span class="h5 text-primary fs-5">{$obj->mProducts[k].discounted_price|string_format:"%.2f €"}</span>
+                            {else}
+                                {* Prix régulier *}
+                                <span class="h5 text-primary fs-5">{$obj->mProducts[k].price|string_format:"%.2f €"}</span>
+                            {/if}
+                        </div>
+                        
+                        <button type="button" class="btn btn-warning btn-sm w-100 p-0">
+                            <i class="bi bi-cart-plus-fill me-1"></i> Ajouter au panier
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    {/section}
+    
 </div>
 {/if}
--- presentation/templates_c/%%10^108^1083067C%%products_list.tpl.php
+++ presentation/templates_c/%%10^108^1083067C%%products_list.tpl.php
@@ -1,33 +1,40 @@
-<?php /* Smarty version 2.6.32, created on 2025-11-02 12:59:58
+<?php /* Smarty version 2.6.32, created on 2025-11-02 15:23:15
          compiled from products_list.tpl */ ?>
 <?php require_once(SMARTY_CORE_DIR . 'core.load_plugins.php');
-smarty_core_load_plugins(array('plugins' => array(array('function', 'load_presentation_object', 'products_list.tpl', 2, false),)), $this); ?>
+smarty_core_load_plugins(array('plugins' => array(array('function', 'load_presentation_object', 'products_list.tpl', 2, false),array('modifier', 'string_format', 'products_list.tpl', 73, false),)), $this); ?>
 <?php echo smarty_function_load_presentation_object(array('filename' => 'products_list','assign' => 'obj'), $this);?>
 
 
 <?php if ($this->_tpl_vars['obj']->mrTotalPages > 1): ?>
-<p class="d-flex">
-Page <?php echo $this->_tpl_vars['obj']->mPage; ?>
- of <?php echo $this->_tpl_vars['obj']->mrTotalPages; ?>
-
-<?php if ($this->_tpl_vars['obj']->mLinkToPreviousPage): ?>
-<a href="<?php echo $this->_tpl_vars['obj']->mLinkToPreviousPage; ?>
-">Précédent</a>
-<?php else: ?>
-Précédent
-<?php endif; ?>
-<?php if ($this->_tpl_vars['obj']->mLinkToNextPage): ?>
-<a href="<?php echo $this->_tpl_vars['obj']->mLinkToNextPage; ?>
-">Suivant</a>
-<?php else: ?>
-Suivant
-<?php endif; ?>
+<p class="d-flex justify-content-center align-items-center my-3">
+    <span class="me-3">Page <?php echo $this->_tpl_vars['obj']->mPage; ?>
+ de <?php echo $this->_tpl_vars['obj']->mrTotalPages; ?>
+</span>
+    
+    <span class="mx-2">
+    <?php if ($this->_tpl_vars['obj']->mLinkToPreviousPage): ?>
+        <a href="<?php echo $this->_tpl_vars['obj']->mLinkToPreviousPage; ?>
+" class="btn btn-outline-secondary btn-sm">Précédent</a>
+    <?php else: ?>
+        <span class="btn btn-outline-secondary btn-sm disabled">Précédent</span>
+    <?php endif; ?>
+    </span>
+    
+    <span class="mx-2">
+    <?php if ($this->_tpl_vars['obj']->mLinkToNextPage): ?>
+        <a href="<?php echo $this->_tpl_vars['obj']->mLinkToNextPage; ?>
+" class="btn btn-outline-secondary btn-sm">Suivant</a>
+    <?php else: ?>
+        <span class="btn btn-outline-secondary btn-sm disabled">Suivant</span>
+    <?php endif; ?>
+    </span>
 </p>
 <?php endif; ?>
 
 <?php if ($this->_tpl_vars['obj']->mProducts): ?>
-<div class="row gap-1">
-<?php unset($this->_sections['k']);
+<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
+    
+    <?php unset($this->_sections['k']);
 $this->_sections['k']['name'] = 'k';
 $this->_sections['k']['loop'] = is_array($_loop=$this->_tpl_vars['obj']->mProducts) ? count($_loop) : max(0, (int)$_loop); unset($_loop);
 $this->_sections['k']['show'] = true;
@@ -51,46 +58,59 @@ $this->_sections['k']['index_next'] = $this->_sections['k']['index'] + $this->_s
 $this->_sections['k']['first']      = ($this->_sections['k']['iteration'] == 1);
 $this->_sections['k']['last']       = ($this->_sections['k']['iteration'] == $this->_sections['k']['total']);
 ?>
-<?php if ($this->_sections['k']['index'] % 2 == 0): ?>
-<?php endif; ?>
-<div class="card col-lg-5">
-  <div>
-    <h3 class="display-6 fs-4">
-    <a href="<?php echo $this->_tpl_vars['obj']->mProducts[$this->_sections['k']['index']]['link_to_product']; ?>
-" class="text-decoration-none text-secondary">
-      <?php echo $this->_tpl_vars['obj']->mProducts[$this->_sections['k']['index']]['name']; ?>
-
-    </a>
-  </h3>
-  </div>
-  <div class="d-flex gap-2">
-    <?php if ($this->_tpl_vars['obj']->mProducts[$this->_sections['k']['index']]['thumbnail'] != ""): ?>
-    <a href="<?php echo $this->_tpl_vars['obj']->mProducts[$this->_sections['k']['index']]['link_to_product']; ?>
+                <div class="col">
+            <div class="card h-100 shadow-sm product-card-custom">
+                
+                                <div class="position-relative">
+                    <?php if ($this->_tpl_vars['obj']->mProducts[$this->_sections['k']['index']]['thumbnail'] != ""): ?>
+                        <a href="<?php echo $this->_tpl_vars['obj']->mProducts[$this->_sections['k']['index']]['link_to_product']; ?>
 ">
-          <img src="https://mdbootstrap.com/img/new/avatars/8.jpg" alt="" style="width: 109px; height: 150px; margin-right: 16px;" />
-    </a>
-    <?php endif; ?>
-    <div class="d-flex flex-column">
-        <p><?php echo $this->_tpl_vars['obj']->mProducts[$this->_sections['k']['index']]['description']; ?>
-</p>
-        <p class="section">
-            Prix:
-            <?php if ($this->_tpl_vars['obj']->mProducts[$this->_sections['k']['index']]['discounted_price'] != 0): ?>
-            <span class="old-price"><?php echo $this->_tpl_vars['obj']->mProducts[$this->_sections['k']['index']]['price']; ?>
+                                                                                    <img src="https://placehold.co/600x500" class="card-img-top" alt="T-Shirt Dragon Rouge">
+                        </a>
+                        
+                                                <?php if ($this->_tpl_vars['obj']->mProducts[$this->_sections['k']['index']]['discounted_price'] != 0 && $this->_tpl_vars['obj']->mProducts[$this->_sections['k']['index']]['price'] > $this->_tpl_vars['obj']->mProducts[$this->_sections['k']['index']]['discounted_price']): ?>
+                            <span class="badge bg-danger position-absolute top-0 end-0 mt-2 me-2">
+                                SOLDE!
+                            </span>
+                        <?php endif; ?>
+                    <?php endif; ?>
+                </div>
+                
+                                <div class="card-body d-flex flex-column text-center m-0 p-1">
+                    <h5 class="card-title fs-5 m-1">
+                        <a href="<?php echo $this->_tpl_vars['obj']->mProducts[$this->_sections['k']['index']]['link_to_product']; ?>
+" class="text-decoration-none text-dark">
+                            <?php echo $this->_tpl_vars['obj']->mProducts[$this->_sections['k']['index']]['name']; ?>
+
+                        </a>
+                    </h5>
+                    
+                                        <p class="card-text text-muted small text-truncate m-0">
+                        <?php echo $this->_tpl_vars['obj']->mProducts[$this->_sections['k']['index']]['description']; ?>
+
+                    </p>
+                    
+                                        <div class="mt-auto">
+                        <div class="price-section d-flex">
+                            <?php if ($this->_tpl_vars['obj']->mProducts[$this->_sections['k']['index']]['discounted_price'] != 0): ?>
+                                                                <span class="text-muted text-decoration-line-through me-2 fs-5"><?php echo ((is_array($_tmp=$this->_tpl_vars['obj']->mProducts[$this->_sections['k']['index']]['price'])) ? $this->_run_mod_handler('string_format', true, $_tmp, "%.2f €") : smarty_modifier_string_format($_tmp, "%.2f €")); ?>
 </span>
-            <span class="price"><?php echo $this->_tpl_vars['obj']->mProducts[$this->_sections['k']['index']]['discounted_price']; ?>
+                                                                <span class="h5 text-primary fs-5"><?php echo ((is_array($_tmp=$this->_tpl_vars['obj']->mProducts[$this->_sections['k']['index']]['discounted_price'])) ? $this->_run_mod_handler('string_format', true, $_tmp, "%.2f €") : smarty_modifier_string_format($_tmp, "%.2f €")); ?>
 </span>
-            <?php else: ?>
-            <span class="price"><?php echo $this->_tpl_vars['obj']->mProducts[$this->_sections['k']['index']]['price']; ?>
+                            <?php else: ?>
+                                                                <span class="h5 text-primary fs-5"><?php echo ((is_array($_tmp=$this->_tpl_vars['obj']->mProducts[$this->_sections['k']['index']]['price'])) ? $this->_run_mod_handler('string_format', true, $_tmp, "%.2f €") : smarty_modifier_string_format($_tmp, "%.2f €")); ?>
 </span>
-            <?php endif; ?>
-        </p>
-    </div>
-  </div>
-</div>
-<?php if ($this->_sections['k']['index'] % 2 != 0 && ! $this->_sections['k']['first'] || $this->_sections['k']['last']): ?>
-<?php endif; ?>
-<?php endfor; endif; ?>
-</div>
+                            <?php endif; ?>
+                        </div>
+                        
+                        <button type="button" class="btn btn-warning btn-sm w-100 p-0">
+                            <i class="bi bi-cart-plus-fill me-1"></i> Ajouter au panier
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <?php endfor; endif; ?>
+    
 </div>
 <?php endif; ?>
